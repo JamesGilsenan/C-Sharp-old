83eefecf8c821bec150e6a40f852c1267f16423b
JamesGilsenan <67464965+JamesGilsenan@users.noreply.github.com>
2020-07-31 14:31:33 +0100

    Add files via upload

---
+++ Supernatural Inheritance/Supernatural Inheritance/Archmage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supernatural_Inheritance
+{
+    class Archmage : Mage
+    {
+        public Archmage(string title) : base(title)
+        {
+
+        }
+
+        public Archmage(string title, string origin) : base(title, origin)
+        {
+
+        }
+
+        public override Storm CastRainStorm()
+        {
+            Storm storm = new Storm("rain", true, Title);
+
+            return storm;
+        }
+
+        public Storm CastLightningStorm()
+        {
+            Storm storm = new Storm("lightning", true, Title);
+
+            return storm;
+        }
+    }
+}
---
+++ Supernatural Inheritance/Supernatural Inheritance/Mage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supernatural_Inheritance
+{
+    class Mage : Pupil
+    {
+        public Mage(string title) : base(title)
+        {
+
+        }
+
+        public Mage(string title, string origin) : base(title, origin)
+        {
+
+        }
+
+        public virtual Storm CastRainStorm()
+        {
+            Storm storm = new Storm("rain", false, Title);
+
+            return storm;
+        }
+    }
+}
